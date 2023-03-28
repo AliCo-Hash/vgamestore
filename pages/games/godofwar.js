d3dcdeb756f95507eeb8ac07d11cdba2eefbc8a1
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2023-03-28 17:43:02 +0100

    embed youtube to gow page

--- pages/games/godofwar.js
+++ pages/games/godofwar.js
@@ -1,3 +1,11 @@
+import React from "react";
+import YouTube from "react-youtube";
+
 export default function godofwar() {
-  return <h1>gow</h1>;
+  return (
+    <>
+      <h1>God of War</h1>
+      <YouTube videoId="HqQMh_tij0c" />
+    </>
+  );
 }
