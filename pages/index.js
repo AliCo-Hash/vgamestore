32d70418044c6ce390f6d6dd47c6be24d7497696
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2023-03-31 00:29:28 +0100

    added layout compoenent and a Head comp in indexjs

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,21 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Link from "next/link";
+import Layout from "@/components/layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <>
-      <h1>Game Store</h1>
+    <Layout>
+      <Head>
+        <title>Awesome Video Games</title>
+      </Head>
       <h2>
         <Link href="/games/godofwar">
           <Image src="/GodofWarfront.jpg" height={350} width={616} alt="" />
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 }
