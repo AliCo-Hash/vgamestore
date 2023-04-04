72e1b8f4846c207aec7ea960f7527b2cac644403
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2023-04-04 23:42:01 +0100

    navbar cssing

--- components/navbar.js
+++ components/navbar.js
@@ -1,13 +1,13 @@
 import Link from "next/link";
-import styles from "../styles/layout.module.css";
+import styles from "../styles/navbar.module.css";
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link className={styles.home} href="/">
-        Awesome
+    <nav className={styles.background}>
+      <Link className={styles.textHome} href="/">
+        Awesome Games
       </Link>
-      <div>
+      <div className={styles.textRight}>
         <Link href="/cart">Cart</Link>
         <Link href="/signin">Sign in</Link>
       </div>
