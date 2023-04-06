470c2ccde49257313cec61abbb5c31dde8880523
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2023-04-06 18:01:22 +0100

    changed 1 of the game and added cover images for them

--- utils/data.js
+++ utils/data.js
@@ -17,12 +17,12 @@ const data = {
         "In a ravaged civilization, where infected and hardened survivors run rampant, Joel, a weary protagonist, is hired to smuggle 14-year-old Ellie out of a military quarantine zone. However, what starts as a small job soon transforms into a brutal cross-country journey.",
     },
     {
-      name: "Hades",
-      slug: "hades",
+      name: "Ori and the Blind Forest Definitive Edition",
+      slug: "ori-and-the-blind-forest-definitive-edition",
       mainImage: "",
-      price: 20.99,
+      price: 14.99,
       description:
-        "Hades is a god-like rogue-like dungeon crawler that combines the best aspects of Supergiant's critically acclaimed titles, including the fast-paced action of Bastion, the rich atmosphere and depth of Transistor, and the character-driven storytelling of Pyre.",
+        "The forest of Nibel is dying. After a powerful storm sets a series of devastating events in motion, Ori must journey to find courage and confront a dark nemesis to save the forest of Nibel. “Ori and the Blind Forest” tells the tale of a young orphan destined for heroics, through a visually stunning Action-Platformer crafted by Moon Studios. Featuring hand-painted artwork, meticulously animated character performance, a fully orchestrated score and dozens of new features in the Definitive Edition, “Ori and the Blind Forest” explores a deeply emotional story about love and sacrifice, and the hope that exists in us all.",
     },
   ],
 };
