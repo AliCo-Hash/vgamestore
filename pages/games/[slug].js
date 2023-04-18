64bbb2f17513dd62461ff65c79a0c9565eba7b75
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2023-04-18 01:51:28 +0100

    removed redundant import from gameitem and change import in slug.js

--- pages/games/[slug].js
+++ pages/games/[slug].js
@@ -3,7 +3,7 @@ import { Store } from "@/utils/Store";
 import data from "@/utils/data";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import React, { useContext } from "react";
+import { useContext } from "react";
 import styles from "styles/game.module.css";
 
 export default function GameScreen() {
