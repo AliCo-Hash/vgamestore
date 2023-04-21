a422db547fade8ae0584bd05ba2064991cb53ff3
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2023-04-22 00:00:05 +0100

    added react hook to signin page

--- pages/signin.js
+++ pages/signin.js
@@ -1,19 +1,63 @@
 import Layout from "@/components/layout";
 import Link from "next/link";
 import styles from "styles/login.module.css";
+import { useForm } from "react-hook-form";
 
 export default function SignInPage() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const submitHandler = ({ email, password }) => {
+    console.log(email, password);
+  };
+
   return (
     <Layout pageTitle="Sign In">
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit(submitHandler)}>
         <h1 className={styles.title}>Login</h1>
         <div className={styles.inputSections}>
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" autoFocus />
+          <input
+            type="email"
+            {...register("email", {
+              required: "Please enter email",
+              pattern: {
+                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                message: "Please enter valid email",
+              },
+            })}
+            className={styles.inputBox}
+            id="email"
+            autoFocus
+          />
+          {errors.email && (
+            <span className={styles.emailMissingMessage}>
+              {errors.email.message}
+            </span>
+          )}
         </div>
         <div className={styles.inputSections}>
           <label htmlFor="password">Password &nbsp;</label>
-          <input type="password" id="password" autoFocus />
+          <input
+            type="password"
+            {...register("password", {
+              required: "Please enter password",
+              minLength: {
+                value: 8,
+                message: "Password must be 8 or more characters long",
+              },
+            })}
+            id="password"
+            autoFocus
+          />
+          {errors.password && (
+            <span className={styles.emailMissingMessage}>
+              {errors.password.message}
+            </span>
+          )}
         </div>
         <div>
           <button className={styles.signInButton}>Login</button>
