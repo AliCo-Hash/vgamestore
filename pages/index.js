0563f1ae16bb87303c32bd2b2bae48da9f541452
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2023-05-02 23:56:11 +0100

    loading games data via db now

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,31 @@
 import { Inter } from "next/font/google";
 import Layout from "@/components/Layout";
-import data from "@/utils/data";
 import GameItem from "@/components/GameItem";
 import styles from "../styles/homeGame.module.css";
+import Game from "@/models/Game";
+import db from "@/utils/db";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({games}) {
   return (
     <Layout pageTitle="Awesome Games">
       <div className={styles.mainGames}>
-        {data.games.map(game => (
+        {games.map(game => (
           <GameItem game={game} key={game.slug}></GameItem>
         ))}
       </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  await db.connect();
+  const games = await Game.find().lean();
+  const gamesJson = JSON.parse(JSON.stringify(games));
+  return {
+    props: {
+      games: gamesJson,
+    }
+  }
 }
