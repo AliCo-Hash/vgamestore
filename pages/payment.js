9f0a95a75e41ef08873ca654617ae82460d376ab
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2023-05-05 18:26:38 +0100

    added cart summary to payment screen

--- pages/payment.js
+++ pages/payment.js
@@ -3,10 +3,16 @@ import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import styles from "../styles/payment.module.css";
+import { useContext } from "react";
+import { Store } from "@/utils/Store";
 
 export default function Payment() {
   const { data: session, status } = useSession();
   const router = useRouter();
+  const { state } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
 
   if (status === "loading") {
     return <Layout>Loading...</Layout>;
@@ -22,7 +28,17 @@ export default function Payment() {
         <PayPalScriptProvider>
           <PayPalButtons />
         </PayPalScriptProvider>
-        <div>cart summary</div>
+        <div className={styles.cartCard}>
+          <div className={styles.cardTitle}>Cart Summary</div>
+          <div className={styles.cardDesc}>
+            Total Price: £{" "}
+            <span className={styles.totalPrice}>
+              {cartItems
+                .reduce((a, c) => a + c.quantity * c.price, 0)
+                .toFixed(2)}{" "}
+            </span>
+          </div>
+        </div>
       </div>
     </Layout>
   );
