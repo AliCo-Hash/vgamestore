4c967e6c9f6d5042ba1d3237b705da87710cb26b
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2023-05-21 04:21:44 +0100

    rendering order. WIP: need to check if user matches the user order before rending

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -1,5 +1,33 @@
 import Layout from "@/components/Layout";
+import Order from "@/models/Order";
+import db from "@/utils/db";
 
-export default function OrderedScreen() {
-  return <Layout>order screen</Layout>;
+export default function OrderedScreen({ order }) {
+  return (
+    <Layout>
+      <h1>Order</h1>
+      {order &&
+        order.orderItems &&
+        order.orderItems.map((item, index) => (
+          <p key={index}>Order Item Name: {item.name}</p>
+        ))}
+    </Layout>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const { id } = params;
+
+  await db.connect();
+  const order = await Order.findOne({ "paymentDetails.id": id })
+    .lean()
+    .then(order => JSON.parse(JSON.stringify(order)));
+  await db.disconnect();
+
+  return {
+    props: {
+      order,
+    },
+  };
 }
