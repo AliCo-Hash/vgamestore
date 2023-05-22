26f82114e29473ffa1ddeb7203ad6fc891232e36
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2023-05-22 18:01:15 +0100

    only correct user can see the order page

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -1,18 +1,35 @@
 import Layout from "@/components/Layout";
 import Order from "@/models/Order";
 import db from "@/utils/db";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export default function OrderedScreen({ order }) {
-  return (
-    <Layout>
-      <h1>Order</h1>
-      {order &&
-        order.orderItems &&
-        order.orderItems.map((item, index) => (
-          <p key={index}>Order Item Name: {item.name}</p>
-        ))}
-    </Layout>
-  );
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  if (status === "loading") {
+    return <Layout>Loading...</Layout>;
+  }
+
+  if (!session) {
+    router.push("/signin");
+  }
+
+  if (session && order && session.user._id === order.user) {
+    return (
+      <Layout>
+        <h1>Order</h1>
+        {order &&
+          order.orderItems &&
+          order.orderItems.map((item, index) => (
+            <p key={index}>Order Item Name: {item.name}</p>
+          ))}
+      </Layout>
+    );
+  }
+
+  return <Layout>You are not authorized to view this order.</Layout>;
 }
 
 export async function getServerSideProps(context) {
