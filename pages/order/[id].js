61078c0ab2f622cc379964473d45819712aa8844
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2023-05-22 19:07:42 +0100

    changed approach of verifying user, now page does not load if the incorrect user is trying to view it

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -1,38 +1,24 @@
 import Layout from "@/components/Layout";
 import Order from "@/models/Order";
 import db from "@/utils/db";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { getSession } from "next-auth/react";
 
 export default function OrderedScreen({ order }) {
-  const { data: session, status } = useSession();
-  const router = useRouter();
-
-  if (status === "loading") {
-    return <Layout>Loading...</Layout>;
-  }
-
-  if (!session) {
-    router.push("/signin");
-  }
-
-  if (session && order && session.user._id === order.user) {
-    return (
-      <Layout>
-        <h1>Order</h1>
-        {order &&
-          order.orderItems &&
-          order.orderItems.map((item, index) => (
-            <p key={index}>Order Item Name: {item.name}</p>
-          ))}
-      </Layout>
-    );
-  }
-
-  return <Layout>You are not authorized to view this order.</Layout>;
+  return (
+    <Layout>
+      <h1>Order</h1>
+      {order &&
+        order.orderItems &&
+        order.orderItems.map((item, index) => (
+          <p key={index}>Order Item Name: {item.name}</p>
+        ))}
+    </Layout>
+  );
 }
 
 export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
   const { params } = context;
   const { id } = params;
 
@@ -41,6 +27,12 @@ export async function getServerSideProps(context) {
     .lean()
     .then(order => JSON.parse(JSON.stringify(order)));
   await db.disconnect();
+
+  if (!session || session.user._id != order.user) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: {
