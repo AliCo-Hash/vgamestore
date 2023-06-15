17c0e6486cdfd3e51e51c8144b95e74bb8967ede
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2023-06-15 02:47:29 +0100

    first cypress test written

---
+++ cypress/e2e/footer.cy.js
@@ -0,0 +1,8 @@
+describe("Navigation", () => {
+  it("should navigate to the about us page", () => {
+    cy.visit("/");
+    cy.get("a[href*=about]").click();
+    cy.url().should("include", "/about");
+    cy.contains("Populate with about us stuff");
+  });
+});
